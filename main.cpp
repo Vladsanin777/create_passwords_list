139923dff5f543ddc090dcaa6111f00cd27f37e3
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-10-03 23:40:54 +0400

    df

--- main.cpp
+++ main.cpp
@@ -8,23 +8,24 @@
 
 using namespace std;
 
-int long long parallelism_step = 100;
+static ofstream outFile;
 
-static char symbols[91] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]:;\"'<>,.?/";
+static int long long parallelism_step = 100;
 
-void symbol_enumeration_thread(int long long number, int long long parallelism_step){
-	for(int long long number_i = number; number_i<parallelism_step; ++number_i) {
-		stringstream password;
-		int long long number_remainder = 0;
-		while (number_remainder<number_i) {
-			number_remainder = number_i % 91;
-			number_i -= number_remainder;
-			number_i /= 91;
-			password<<symbols[number_remainder];
-		}
-		cout<<(password).str()<<endl;
+static string symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]:;\"'<>,.?/";
+
+static int long long size_symbols = symbols.length();
+
+static void generateCombinations(string current, int long long length) {
+    if (current.size() == length) {
+        outFile << current << endl;
+        return;
+    }
+    
+    for (char ch : symbols){
+		thread t(generateCombinations, current + ch, length);
+		t.detach();
 	}
-	return;
 }
 
 int main(){
@@ -39,14 +40,11 @@ int main(){
 	string file_name;
 	cin>>file_name;
 	system(("touch "+file_name).c_str());
-	ofstream outFile(file_name);
-	int long long min_number, number = 0;
-	min_number = pow(91, min_count_symbols-1);
-	for(min_count_symbols; max_count_symbols < min_count_symbols; ++min_count_symbols) number += pow(80, min_count_symbols);
-	for(min_number; number<min_number;){
-		thread t(symbol_enumeration_thread, min_number, min_number+parallelism_step);
+	outFile.open(file_name);
+	outFile.is_open();
+    for (min_count_symbols; max_count_symbols>min_count_symbols ; ++min_count_symbols) {
+        thread t(generateCombinations, "", min_count_symbols);
 		t.detach();
-		min_number += parallelism_step;
-	}
+    }
 	return 0;
 }
